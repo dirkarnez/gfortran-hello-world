81c7f06d3f396c9ce6865167d9a0f743d2155b3d
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-30 23:09:49 +0800

    - add local-build.cmd

---
+++ local-build.cmd
@@ -0,0 +1,12 @@
+@REM run as Administrator
+@echo off
+cd /d %~dp0
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
+
+SET PATH=^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin;
+
+gfortran helloworld.f90 -o helloworld
